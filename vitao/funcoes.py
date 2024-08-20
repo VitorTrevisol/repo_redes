d8c09f35853f29f1a56e2116c9e1c14d03353203
VitorTrevisol <franciscovitoor2@gmail.com>
2024-08-20 18:33:19 -0300

    a

--- vitao/funcoes.py
+++ vitao/funcoes.py
@@ -1,79 +1,120 @@
-import socket
-import threading
-import time
 import sqlite3
-import random
 
 def conectar_banco():
     return sqlite3.connect('banco.db')
 
-def consultar_pessoa(nome_consulta, sobrenome_consulta):
+def adicionar_cliente(id, nome, sobrenome, grupos):
     conexao = conectar_banco()
     cursor = conexao.cursor()
     cursor.execute('''
-    SELECT id FROM clientes 
-    WHERE nome = ? AND sobrenome = ?
-    ''', (nome_consulta, sobrenome_consulta))
-    resultado = cursor.fetchone()
-    print(resultado)
-    conexao.close()
-    if resultado:
-        return resultado[0]
-    return None
-
-def adicionar_pessoa(nome, sobrenome):
-    id_cliente = random.randint(10**12, 10**13 - 1)
-    conexao = conectar_banco()
-    cursor = conexao.cursor()
-    
-    cursor.execute('''
-    INSERT INTO clientes (id, nome, sobrenome)
-    VALUES (?, ?, ?)
-    ''', (id_cliente, nome, sobrenome))
-    
+    INSERT INTO clientes (id, nome, sobrenome, grupos)
+    VALUES (?, ?, ?, ?)
+    ''', (id, nome, sobrenome, grupos))
     conexao.commit()
     conexao.close()
 
-    return id_cliente
-
-def mensagemEspera(id):
+def remover_cliente(id):
     conexao = conectar_banco()
     cursor = conexao.cursor()
     cursor.execute('''
-    SELECT * FROM mensagemEspera
-    WHERE destinatario = ? 
+    DELETE FROM clientes WHERE id = ?
+    ''', (id,))
+    conexao.commit()
+    conexao.close()
+
+def consultar_cliente(id):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''
+    SELECT * FROM clientes WHERE id = ?
     ''', (id,))
     resultado = cursor.fetchone()
     conexao.close()
     return resultado
 
-def mensagensAntigas(id):
+def adicionar_conexao(id, idUm, idDois, mensagens):
     conexao = conectar_banco()
     cursor = conexao.cursor()
     cursor.execute('''
-    SELECT * FROM conexoes
-    WHERE idUm = ? 
+    INSERT INTO conexoes (id, idUm, idDois, mensagens)
+    VALUES (?, ?, ?, ?)
+    ''', (id, idUm, idDois, mensagens))
+    conexao.commit()
+    conexao.close()
+
+def remover_conexao(id):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''
+    DELETE FROM conexoes WHERE id = ?
+    ''', (id,))
+    conexao.commit()
+    conexao.close()
+
+def consultar_conexao(id):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''
+    SELECT * FROM conexoes WHERE id = ?
     ''', (id,))
     resultado = cursor.fetchone()
     conexao.close()
     return resultado
 
-def grupos(id):
+def adicionar_grupo(id, nome, mensagens):
     conexao = conectar_banco()
     cursor = conexao.cursor()
     cursor.execute('''
-    SELECT * FROM clientes
-    WHERE id = ? 
+    INSERT INTO grupos (id, nome, mensagens)
+    VALUES (?, ?, ?)
+    ''', (id, nome, mensagens))
+    conexao.commit()
+    conexao.close()
+
+def remover_grupo(id):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''
+    DELETE FROM grupos WHERE id = ?
     ''', (id,))
-    idGrupos = cursor.fetchone()
-    idGrupos = idGrupos[3].strip('[]').split(', ')
-    listaGrupos = []
-    for x in idGrupos:
-        cursor.execute('''
-        SELECT * FROM grupos
-        WHERE id = ? 
-        ''', (int(x),))
-        resultado = cursor.fetchone()
-        listaGrupos.append(resultado)
+    conexao.commit()
+    conexao.close()
+
+def consultar_grupo(id):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''
+    SELECT * FROM grupos WHERE id = ?
+    ''', (id,))
+    resultado = cursor.fetchone()
+    conexao.close()
+    return resultado
+
+def adicionar_mensagem_espera(destinatario, remetente, mensagens):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''
+    INSERT INTO mensagemEspera (destinatario, remetente, mensagens)
+    VALUES (?, ?, ?)
+    ''', (destinatario, remetente, mensagens))
+    conexao.commit()
+    conexao.close()
+
+def remover_mensagem_espera(destinatario, remetente):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''
+    DELETE FROM mensagemEspera WHERE destinatario = ? AND remetente = ?
+    ''', (destinatario, remetente))
+    conexao.commit()
+    conexao.close()
+
+def consultar_mensagem_espera(destinatario):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''
+    SELECT * FROM mensagemEspera WHERE destinatario = ?
+    ''', (destinatario,))
+    resultado = cursor.fetchall()
     conexao.close()
     return resultado
